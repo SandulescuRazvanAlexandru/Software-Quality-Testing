948887a9970ce17c412575a87e9735774bf33d2b
RazvanSandulescu <sandulescu.razvan16@gmail.com>
2021-02-27 13:14:03 +0200

    commit with giraffe

--- Seminar_1/src/program/Main.java
+++ Seminar_1/src/program/Main.java
@@ -3,6 +3,13 @@ package program;
 public class Main {
 
     public static void main(String[] args) {
-        // my code goes here
+        Zoo zoo = new Zoo();
+        Girafa g1 = new Girafa("Girafa 1");
+        Girafa g2 = new Girafa("Girafa 2");
+
+        zoo.Add(g1);
+        zoo.Add(g2);
+
+        zoo.feedAllAnimals();
     }
 }
---
+++ Seminar_1/src/program/Zoo.java
@@ -0,0 +1,29 @@
+package program;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo {
+    private Zookeeper zookeeper;
+    private List<Animal> animals;
+
+    public Zoo() {
+        this.zookeeper = new Zookeeper("Gigel");
+        this.animals = new ArrayList<>();
+    }
+
+    public Zoo(Zookeeper zookeeper, List<Animal> animalList) {
+        this.zookeeper = zookeeper;
+        this.animals = animalList;
+    }
+
+    public void Add(Animal animal) {
+        this.animals.add(animal);
+    }
+
+    public void feedAllAnimals() {
+        for (Animal a: animals) {
+            zookeeper.feed(a);
+        }
+    }
+}
