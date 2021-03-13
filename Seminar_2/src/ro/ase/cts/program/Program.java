47323054d07ac1607afbaf4fd266de8a65059438
RazvanSandulescu <sandulescu.razvan16@gmail.com>
2021-03-13 12:23:45 +0200

    Seminar_2 + 3

--- Seminar_2/src/ro/ase/cts/program/Program.java
+++ Seminar_2/src/ro/ase/cts/program/Program.java
@@ -1,31 +1,27 @@
 package ro.ase.cts.program;
 
-import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
-import ro.ase.cts.clase.Student;
-import ro.ase.cts.clase.readers.AngajatiReader;
 import ro.ase.cts.clase.readers.EleviReader;
 import ro.ase.cts.clase.readers.Readable;
-import ro.ase.cts.clase.readers.StudentiReader;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
 public class Program {
 
-	public static List<Aplicant> citesteAplicanti(String numeFisier, Readable reader) throws FileNotFoundException {
-		return reader.readAplicant(numeFisier);
-	}
+    public static List<Aplicant> citesteAplicanti(Readable reader) throws FileNotFoundException {
+        return reader.readAplicanti();
+    }
 
-	public static void main(String[] args) {
-		List<Aplicant> listaAplicant;
-		try {
-			listaAplicant = citesteAplicanti("Seminar_2/elevi.txt", new EleviReader());
-			for(Aplicant aplicant:listaAplicant)
-				System.out.println(aplicant.toString());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        List<Aplicant> listaAplicant;
+        try {
+            listaAplicant = citesteAplicanti(new EleviReader("Seminar_2/elevi.txt"));
+            for (Aplicant aplicant : listaAplicant)
+                System.out.println(aplicant.toString());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
