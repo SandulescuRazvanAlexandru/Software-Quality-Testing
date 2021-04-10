0a5e7fb1184ed40e61235726e78354c695be4997
RazvanSandulescu <sandulescu.razvan16@gmail.com>
2021-04-10 12:13:12 +0300

    Adapters

---
+++ Seminar_7/src/ro/ase/cts/program/Main.java
@@ -0,0 +1,29 @@
+package ro.ase.cts.program;
+
+import ro.ase.cts.clase.AdapterLeasing;
+import ro.ase.cts.clase.AdapterLeasingClase;
+import ro.ase.cts.clase.Creditable;
+import ro.ase.cts.clase.Leasing;
+
+public class Main {
+    public static void printeazaInfo(Creditable credit) {
+        credit.crediteaza();
+    }
+
+    public static void main(String[] args) {
+        Leasing leasing = new Leasing("Bogdan", 10000);
+        //printeazaInfo(leasing);
+
+        AdapterLeasing adapterLeasing = new AdapterLeasing(leasing);
+        adapterLeasing.crediteaza();
+
+        System.out.println("-------------------------------------------------- Adapter obiecte --------------------------------------------------\n");
+        printeazaInfo(adapterLeasing);
+
+        System.out.println("-------------------------------------------------- Adapter clase ----------------------------------------------------\n");
+        AdapterLeasingClase adapterLeasingClase = new AdapterLeasingClase("Gigel", 123456);
+        printeazaInfo(adapterLeasingClase);
+
+
+    }
+}
