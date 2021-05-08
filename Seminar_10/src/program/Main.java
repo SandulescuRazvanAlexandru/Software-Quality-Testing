27ebfa059e60326e75a23701320b59e825407f74
RazvanSandulescu <sandulescu.razvan16@gmail.com>
2021-05-08 12:58:13 +0300

    Command + Chain

--- Seminar_10/src/program/Main.java
+++ Seminar_10/src/program/Main.java
@@ -1,6 +1,10 @@
 package program;
 
 import clase.*;
+import claseChain.Cont;
+import claseChain.ContCredit;
+import claseChain.ContCurent;
+import claseChain.ContEconomii;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,5 +25,33 @@ public class Main {
         managerComenzi.executaComanda();
         managerComenzi.executaComanda();
 
+
+        System.out.println("---------------------------------------------------------------");
+
+//        Cont contCurent = new ContCurent(200);
+//        Cont contCredit = new ContCredit(150);
+//        Cont contEconomii = new ContEconomii(150);
+//
+//        contCurent.setSuccesorCont(contCredit);
+//        contCurent.setSuccesorCont(contEconomii);
+//
+//        contCurent.realizeazaPlata(100);
+//        contCurent.realizeazaPlata(150);
+//        contCurent.realizeazaPlata(100);
+//        contCurent.realizeazaPlata(100);
+//        contCurent.realizeazaPlata(100);
+
+        Cont contCurent = new ContCurent(200);
+        Cont contCredit = new ContCredit(150);
+        Cont contEconomii = new ContEconomii(150);
+
+        contCurent.setSuccesorCont(contCredit);
+        contCredit.setSuccesorCont(contEconomii);
+
+        contCurent.realizeazaPlata(100);
+        contCurent.realizeazaPlata(150);
+        contCurent.realizeazaPlata(100);
+        contCurent.realizeazaPlata(100);
+        contCurent.realizeazaPlata(100);
     }
 }
